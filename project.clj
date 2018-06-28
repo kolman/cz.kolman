27a4d8ca8b2aa14c0e9840f5f81631fbebb4b2d3
Daniel Kolman <dkolman@vendavo.com>
2018-06-28 08:53:54 -0600

    Simple routing with bidi/pushy

--- project.clj
+++ project.clj
@@ -4,14 +4,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  
-  
   :min-lein-version "2.7.1"
 
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.238"]
                  [org.clojure/core.async  "0.4.474"]
-                 [rum "0.11.2"]]
+                 [rum "0.11.2"]
+                 [bidi "2.1.3"]
+                 [kibu/pushy "0.3.8"]]
 
   :plugins [[lein-figwheel "0.5.16"]
             [lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]]
--- src/cz/kolman/core.cljs
+++ src/cz/kolman/core.cljs
@@ -1,21 +1,57 @@
 (ns cz.kolman.core
-    (:require [rum.core :as rum]))
+  (:require [rum.core :as rum]
+            [bidi.bidi :as bidi]
+            [pushy.core :as pushy]))
 
 (enable-console-print!)
 
 (println "This text is printed from src/cz.kolman/core.cljs. Go ahead and edit it and see reloading in action.")
 
+;; routing
+
+(def routes ["/" {""      :home
+                  "about" :about}])
+
+(defonce page (atom :home))
+
+(defn- parse-url [url]
+  (bidi/match-route routes url))
+
+(defn- dispatch-route [matched-route]
+  (let [panel-name (keyword (str (name (:handler matched-route))))]
+    (reset! page panel-name)))
+
+(defn app-routes []
+  (pushy/start! (pushy/pushy dispatch-route parse-url)))
+
+(def url-for (partial bidi/path-for routes))
+
 ;; define your app data so that it doesn't get over-written on reload
 
 (defonce app-state (atom {:text "Hello world!"}))
 
+;; views
 
 (rum/defc hello-world []
   [:div
    [:h1 (:text @app-state)]
-   [:h3 "Welcome to brand new website!"]])
+   [:h3 "Welcome to brand new website!"]
+   [:p [:a {:href (url-for :about)} "About"]]])
 
-(rum/mount (hello-world)
+(rum/defc about []
+  [:div
+   [:h1 (:text @app-state)]
+   [:h3 "About this site..."]
+   [:p [:a {:href (url-for :home)} "Overview"]]])
+
+(rum/defc app < rum/reactive []
+  (case (rum/react page)
+    :home (hello-world)
+    :about (about)))
+
+(app-routes)
+
+(rum/mount (app)
            (. js/document (getElementById "app")))
 
 (defn on-js-reload []
